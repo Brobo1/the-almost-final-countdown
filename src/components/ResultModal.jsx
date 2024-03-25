e270794b6e4d241006c3b5ebd4c794a043090ef7
brobo1 <armanahz95@gmail.com>
2024-03-25 17:07:23 +0100

    Adjust scoring calculation in ResultModal.jsx
    Altered the mathematical formula used for determining the user's score in ResultModal.jsx. This change ensures the score is calculated properly, addressing an issue where scores were being magnified.

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -9,7 +9,7 @@ const ResultModal = forwardRef(function ResultModal(
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
-  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 1000);
+  const score = Math.round((1 - remainingTime / (targetTime * 10000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
