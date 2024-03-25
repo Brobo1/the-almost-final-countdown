a612eaa1d882154d2aa0df77010af2983554fbc3
brobo1 <armanahz95@gmail.com>
2024-03-25 17:05:18 +0100

    Refactor ResultModal.jsx for cleaner syntax
    Updated the formatting of ResultModal.jsx component to improve readability and maintainability. Changes include use of clearer syntax, adjusting white spaces and semicolons, and cleaning up the scoring logic.

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,52 +1,41 @@
-import {forwardRef, useImperativeHandle, useRef} from "react";
-import {createPortal} from "react-dom";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
-const ResultModal = forwardRef(function ResultModal({
-														remainingTime,
-														targetTime,
-														onReset
-													}, ref) {
-	const dialog = useRef(null);
-	const userLost = remainingTime <= 0;
-	const formattedRemainingTime = (
-		remainingTime / 1000
-	).toFixed(2);
+const ResultModal = forwardRef(function ResultModal(
+  { remainingTime, targetTime, onReset },
+  ref,
+) {
+  const dialog = useRef(null);
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
-	const score = Math.round((
-								 1 - remainingTime / (
-									   targetTime * 1000
-								   )
-							 ) * 100);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 1000);
 
-	useImperativeHandle(ref, () => {
-		return {
-			open() {
-				dialog.current.showModal();
-			}
-		}
-	});
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
 
-	return createPortal(<dialog
-		ref={dialog}
-		className={"result-modal"}
-		onClose={onReset}
-	>
-		{userLost && <h2>You lost</h2>}
-		{!userLost && <h2>Your Score: {score}</h2>}
-		<p>
-			The target time was <strong>{targetTime} seconds.</strong>
-		</p>
-		<p>
-			You stopped the timer with{' '}
-			<strong>{formattedRemainingTime} seconds left.</strong>
-		</p>
-		<form
-			method={"dialog"}
-			onSubmit={onReset}
-		>
-			<button>Close</button>
-		</form>
-	</dialog>, document.getElementById("modal"));
+  return createPortal(
+    <dialog ref={dialog} className={"result-modal"} onClose={onReset}>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method={"dialog"} onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal"),
+  );
 });
 
 export default ResultModal;
